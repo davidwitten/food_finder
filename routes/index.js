45141102094ea5dc960be2574a8d1bf1f1abb20c
davidwitten <dwitt12345@gmail.com>
2020-05-07 15:16:11 -0400

    D

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+
+let index = require('../controllers/index');
+
+/* GET home page. */
+router.get('/', index.index);
+
+module.exports = router;
